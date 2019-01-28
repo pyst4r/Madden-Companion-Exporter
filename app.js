802e20bbdabc5fd9c2bca1db1adb7f71884117f0
Manuel Zimmer <pyst4r@users.noreply.github.com>
2019-01-28 14:03:04 +0100

    Update app.js with database info

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ var admin = require("firebase-admin");
 
 const app = express();
 
-// TODO: Enter the path to your service account json file
-// Need help with this step go here: https://firebase.google.com/docs/admin/setup
-// const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
-
-// TODO: Enter your database url from firebase
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
-// });
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: 'madden-germany-nfl-export',
+    clientEmail: 'foo@madden-germany-nfl-export.iam.gserviceaccount.com',
+    privateKey: '-----BEGIN PRIVATE KEY-----\nAIzaSyCVlehJ-rnd-jahcACb_BQxEkG2H6uCloM\n-----END PRIVATE KEY-----\n'
+  }),
+  databaseURL: 'https://madden-germany-nfl-export.firebaseio.com'
+});
 
 
 app.set('port', (process.env.PORT || 3001));
